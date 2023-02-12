e9354565635a508a3b7d370a2d2189c733df5e9d
NghiepRadeon <nghiepradeon@gmail.com>
2023-02-12 16:53:02 +0700

    swagger category

--- src/categories/category.controller.ts
+++ src/categories/category.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiCookieAuth, ApiTags } from '@nestjs/swagger';
 import { Roles } from 'src/auth/decorators/roles.decorator';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
@@ -6,10 +7,14 @@ import { Role } from 'src/utils/types';
 import { CategoryService } from './category.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 
+@ApiTags('Category')
 @Controller('category')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
+  @ApiTags('Admin')
+  @ApiBearerAuth('Bearer Token')
+  @ApiCookieAuth('access_token')
   @Post()
   @Roles(Role.Admin)
   @UseGuards(JwtAuthGuard, RolesGuard)
